be0c69619c889f889ae4a4c390519915db75f6ce
Jfelix61 <jfelix61@outlook.com>
2020-06-27 19:31:22 -0400

    Added general logic skeleton and javaFX layout

---
+++ src/proyectojflex/Controller.java
@@ -0,0 +1,26 @@
+package proyectojflex;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+public class Controller {
+    @FXML
+    private TextArea expressionsTextArea;
+
+    @FXML
+    private ListView<String> resultListView;
+
+    public void clearForm() {
+        expressionsTextArea.clear();
+    }
+
+    public void analyze() {
+        ObservableList<String> names = FXCollections.observableArrayList(
+                "Julia\nHEY\nHEYY", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
+        resultListView.setItems(names);
+        // TODO: add logic here
+    }
+}
